a139734040f407ed08f71b385571edf4b308ba25
jaso21 <116608193+jaso21@users.noreply.github.com>
2022-10-25 17:45:20 -0400

    modificando el archivo java script

--- Resources/form.js
+++ Resources/form.js
@@ -4,3 +4,44 @@ function sub() {
 
     document.getElementById('usu').innerHTML = usuario;
 }
+function cambio() {
+    var dol = 6.98;
+    var re;
+    var pe = parseFloat(document.getElementById('bs').value,20);
+    re = pe / dol;
+    document.getElementById('resul').value = re.toFixed(2);
+
+}
+var valor = document.getElementById("valor");
+valor.addEventListener('keyup',convertir);
+txtResultado = document.getElementById("resutado");
+unidad1 = document.getElementById("unidad1");
+
+unidad1.addEventListener('change',convertir);
+unidad2 = document.getElementById("unidad2");
+unidad2.addEventListener('change',convertir);
+var resultado
+function convertir() {
+ if (valor.value==''){
+
+    return;
+ }
+ numero = valor.value;
+ numero = parseFloat(numero);
+ if (unidad1.value == 'Bs' && unidad2.value  == 'Bs'){
+   resultado = numero * 1;
+   txtResultado.innerHTML= numero + " Bs == " + resultado.toFixed(2) +" Bs";
+}
+if (unidad1.value == 'dolar' && unidad2.value  == 'dolar'){
+   resultado = numero * 1;
+   txtResultado.innerHTML= numero + " dolares " + resultado.toFixed(2) +" dolares";
+}
+if (unidad1.value == 'Bs' && unidad2.value  == 'dolar'){
+   resultado = numero / 6.86;
+   txtResultado.innerHTML= numero + " Bs == " + resultado.toFixed(2) +" dolares";
+}
+if (unidad1.value == 'dolar' && unidad2.value  == 'Bs'){
+   resultado = numero * 6.86;
+   txtResultado.innerHTML= numero + " dolar == " + resultado.toFixed(2) +" Bs";
+}
+}
